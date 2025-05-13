fb413358a89cd6ff838e1491691c23d6abfabd62
zhangyueyang <15832219176@163.com>
2025-05-13 18:22:09 +0800

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    `maven-publish`
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.example.mylibrary"
+            artifactId = "mylibrary"
+            version = "v1.0"
+
+//            from(components["java"])
+        }
+    }
 }
